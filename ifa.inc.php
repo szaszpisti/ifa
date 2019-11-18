caaca363f50c263464a0599c55c8c106bb249699
Szász Imre <szaszi@szepi.hu>
2019-11-18 18:53:05 +0100

    meta viewport

--- ifa.inc.php
+++ ifa.inc.php
@@ -194,6 +194,7 @@ print <<< EnD
   <meta name="Author" content="Szász Imre">
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <meta http-equiv="Expires" content="Tue, 20 Aug 1996 14:25:27 GMT">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="Content-Language" content="hu">
   <link rel="stylesheet" href="$css.css" type="text/css">
 </head>
